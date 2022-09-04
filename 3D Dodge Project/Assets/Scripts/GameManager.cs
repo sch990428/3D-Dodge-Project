96d61d8a84a092f9114973a63bc6b2869fdab6c3
Changhun Song <990428sch@naver.com>
2022-09-04 20:54:04 +0900

    스코어 점수 및 게임오버 등 UI 추가

---
+++ 3D Dodge Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameoverText;
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI recordText;
+
+    private float surviveTime;
+    private bool isGameover;
+
+    void Start()
+    {
+        surviveTime = 0;
+        isGameover = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGameover)
+        {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time : " + (int)surviveTime;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        isGameover = true;
+
+        gameoverText.SetActive(true);
+
+        float bestTime = PlayerPrefs.GetFloat("BestTime");
+
+        if (surviveTime > bestTime)
+        {
+            bestTime = surviveTime;
+
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+        }
+
+        recordText.text = "Highscore : " + (int)bestTime;
+    }
+}
